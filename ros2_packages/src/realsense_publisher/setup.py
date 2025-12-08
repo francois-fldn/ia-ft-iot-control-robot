7c85fcdce4e6b17ab2c7161a69ad7014df63dbc8
Arnaud Acquatella <arnaud@macbookpro.home>
2025-12-08 19:46:10 +0100

    detection 3D de la balle et models yolov5 finetuné

--- ros2_packages/src/realsense_publisher/setup.py
+++ ros2_packages/src/realsense_publisher/setup.py
@@ -14,6 +14,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('lib', package_name), glob(package_name + '/yolov5/*.tflite')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -29,6 +30,7 @@ setup(
     entry_points={
         'console_scripts': [
         		'realsense_publisher = realsense_publisher.realsense_p:main',
+                'ball_detector = realsense_publisher.BallDetector3D:main',
         ],
     },
 )
