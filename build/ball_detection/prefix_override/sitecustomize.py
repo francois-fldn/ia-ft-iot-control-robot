edf9239055614213771d21ae1cfc28526e2f849c
omelette-bio <flandinfrancois@proton.me>
2025-12-10 11:23:12 +0100

    enlevé les dossiers build et install

--- build/ball_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/francois/00-projets/ia-ft-iot-control-robot/install/ball_detection'
--- build/turtlebot3_descriptions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francois/00-projets/ia-ft-iot-control-robot/src/turtlebot3_descriptions
-# Build directory: /home/francois/00-projets/ia-ft-iot-control-robot/build/turtlebot3_descriptions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
