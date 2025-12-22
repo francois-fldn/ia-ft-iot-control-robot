f78d55a6787086ac30426b9b63389e225204e5d1
omelette-bio <flandinfrancois@proton.me>
2025-12-22 13:43:49 +0100

    ahhahahahaha le modele complet de la balle est fini

--- src/turtlebot3_gazebo/launch/projects/projects_house_world.launch.py
+++ src/turtlebot3_gazebo/launch/projects/projects_house_world.launch.py
@@ -21,8 +21,8 @@ def generate_launch_description():
     )
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    x_pose = LaunchConfiguration('x_pose', default='2.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.5')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
